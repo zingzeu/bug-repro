468ec4bdc72705f640f72ed110b72f75aa77ebce
Radium <2714446+ztl8702@users.noreply.github.com>
2023-01-14 12:17:03 +0000

    without package extension. public hoisting is working for pnpm
     but not under bazel

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
-
 const config = {
+  compiler: 'webpack5',
   projectName: 'miniapp',
   date: '2020-8-2',
   designWidth: 750,
@@ -45,21 +45,7 @@ const config = {
         },
       },
     },
-  },
-  h5: {
-    publicPath: '/',
-    staticDirectory: 'static',
-    postcss: {
-      autoprefixer: { enable: true, config: {} },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]',
-        },
-      },
-    },
-  },
+  }
 };
 
 module.exports = function (merge) {
