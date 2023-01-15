26c2d0d652f3cd2e259e18f6f738da4f7e5d6491
Radium <2714446+ztl8702@users.noreply.github.com>
2023-01-15 06:36:18 +0000

    save

--- app/config/index.js
+++ app/config/index.js
@@ -18,8 +18,9 @@ const config = {
     ],
   },
   framework: 'react',
-  alias: {
-    '@': path.resolve(__dirname, '..', 'src'),
+  compiler: 'webpack5',
+  cache: {
+    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   sass: {
     resource: ['src/styles/variables.scss', 'src/styles/mixins.scss'],
